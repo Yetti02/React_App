7c73213fa573ecceede77c7b3d76874f1f0f8953
yetti02 <quadriyetunde04@gmail.com>
2025-01-21 23:49:26 +0100

    more changes

--- yetti/src/App.js
+++ yetti/src/App.js
@@ -1,5 +1,6 @@
 // src/App.js  
 import React from 'react';  
+import './App.css'; 
 import Header from './Header';  
 import Footer from './Footer';  
 import Content from './Content';  
@@ -7,13 +8,11 @@ import Content from './Content';
 function App() {  
     return (  
         <div>  
-            {/* Pass props to the Header component */}  
+           
             <Header title="Welcome to My App" subtitle="This is a simple React app" />  
-
-            {/* Add the Content component */}  
+ 
             <Content text="Created with react" />  
-
-            {/* Pass props to the Footer component */}  
+  
             <Footer text="© 2025 My App. All rights reserved." />  
         </div>  
     );  
--- yetti/src/Content.js
+++ yetti/src/Content.js
@@ -1,28 +1,69 @@
-// src/Content.js  
-import React, { Component } from 'react';  
+//  import React, { Component } from 'react';  
 
-class Content extends Component {  
-    constructor(props) {  
-        super(props);  
-        this.state = {  
-            content: 'This is the initial content of the page.'  
-        };  
-    }  
+// class Content extends Component {  
+//     constructor(props) {  
+//         super(props);  
+//         this.state = {  
+//             content: 'This is the initial page content.'  
+//         };  
+//     }  
 
-    updateContent = () => {  
-        this.setState({  
-            content: 'The content has been updated!'  
-        });  
-    }  
+//     updateContent = () => {  
+//         this.setState({  
+//             content: 'The content has been updated!'  
+//         });  
+//     }  
 
-    render() {  
-        return (  
-            <div>  
-                <p>{this.state.content}</p>  
-                <button onClick={this.updateContent}>Update Content</button>  
-            </div>  
-        );  
-    }  
-}  
+//     render() {  
+//         return (  
+//             <div className='container'>  
+//                 <p>{this.state.content}</p>  
+//                 <button onClick={this.updateContent}>Updated Content</button>  
+//             </div>  
+//         );  
+//     }  
+// }  
+
+// export default Content;
+
+
+  
+import React, { useState } from 'react';  
+
+const Content = () => {  
+    const [items, setItems] = useState([]);    
+    const [inputValue, setInputValue] = useState('');  
+
+    const handleChange = (event) => {  
+        setInputValue(event.target.value);   
+    };  
+
+    const addItem = () => {  
+        if (inputValue.trim()) {  
+            setItems([...items, inputValue]);   
+            setInputValue(''); 
+        }  
+    };  
+
+    return (  
+        <div>  
+            <h3 className='contain'>Manage Items</h3>  
+            <input   
+                type="text"   
+                value={inputValue}   
+                onChange={handleChange}   
+                placeholder="Add an item"  
+            />  
+            <div className='contain'>
+            <button onClick={addItem}>Add Item</button>  
+            </div>
+            <ul>  
+                {items.map((item, index) => (  
+                    <li key={index}>{item}</li>   
+                ))}  
+            </ul>  
+        </div>  
+    );  
+};  
 
 export default Content;
